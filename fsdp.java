84b3c27761be6b9310a1b7fb40f80f5e5f02b7d5
udayprakashspt <129489345+udayprakashspt@users.noreply.github.com>
2023-09-06 22:51:29 -0700

    Update fsdp.java

--- fsdp.java
+++ fsdp.java
@@ -3,6 +3,6 @@ class fsdp
 	 public static void main (String args [])
 	 {
 		 System.out.println("welcome to full stack devolopment");
-		  System.out.println("welcome to java program");
+		  System.out.println("java program is ready to execute");
          }
   }
