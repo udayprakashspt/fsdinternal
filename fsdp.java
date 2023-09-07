0f475b160382c57f9f4b88aa7c6adfab79669ff5
uday <uday123@gmail.com>
2023-09-06 22:44:13 -0700

    change2

--- fsdp.java
+++ fsdp.java
@@ -2,6 +2,7 @@ class fsdp
  {
 	 public static void main (String args [])
 	 {
-		 System.out.println("welcome to full stack");
+		 System.out.println("welcome to full stack devolopment");
+		  System.out.println("welcome to java program");
          }
   }
